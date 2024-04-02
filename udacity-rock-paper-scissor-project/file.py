fc6af4c11fa983949f6c93c6916a7711dc6c73a0
aliciaantunes <159080104+aliciaantunes@users.noreply.github.com>
2024-04-02 18:32:15 +0100

    Add files via upload

--- udacity-rock-paper-scissor-project/file.py
+++ udacity-rock-paper-scissor-project/file.py
@@ -8,21 +8,28 @@ def print_pause(message):
 
 
 class Player:
+    def __init__(self):
+        self.last_move = None
+
     def move(self):
         return 'rock'
 
     def learn(self, my_move, their_move):
-        pass
+        self.last_move = their_move
 
-    def choose_move(self):
+
+class HumanPlayer(Player):
+    def move(self):
         while True:
-            move = input("Choose: rock, paper, or scissors? ").lower()
-            if move in ['rock', 'paper', 'scissors']:
-                return move
+            choice = input("Choose rock, paper, or scissors: ").lower()
+            if choice in ['rock', 'paper', 'scissors']:
+                return choice
             else:
-                print_pause("Invalid move. "
-                            "Please choose "
-                            "'rock', 'paper', or 'scissors'.")
+                print_pause("Invalid choice. Please choose again.")
+
+
+class RockerPlayer(Player):
+    pass  # Inherits move method from Player, always returns 'rock'
 
 
 class RandomPlayer(Player):
@@ -30,118 +37,89 @@ class RandomPlayer(Player):
         return random.choice(['rock', 'paper', 'scissors'])
 
 
-class ReflectPlayer(Player):
-    def __init__(self):
-        self.last_move = None
-
+class MimicPlayer(Player):
     def move(self):
         if self.last_move is None:
             return random.choice(['rock', 'paper', 'scissors'])
         return self.last_move
 
-    def learn(self, my_move, their_move):
-        self.last_move = their_move
-
 
 class CyclePlayer(Player):
     def __init__(self):
-        self.cycle_moves = ['rock', 'paper', 'scissors']
+        super().__init__()
+        self.moves = ['rock', 'paper', 'scissors']
         self.index = 0
 
     def move(self):
-        move = self.cycle_moves[self.index]
-        self.index = (self.index + 1) % len(self.cycle_moves)
-        return move
-
-    def learn(self, my_move, their_move):
-        self.last_move = their_move
+        choice = self.moves[self.index]
+        self.index = (self.index + 1) % len(self.moves)
+        return choice
 
 
-class AllRockerPlayer(Player):
-    def move(self):
-        return 'rock'
-
-
-class HumanPlayer(Player):
-    pass
+BEATS = {'rock': 'scissors',
+         'scissors': 'paper',
+         'paper': 'rock'}
 
 
 def beats(one, two):
-    return ((one == 'rock' and two == 'scissors') or
-            (one == 'scissors' and two == 'paper') or
-            (one == 'paper' and two == 'rock'))
+    return BEATS[one] == two
 
 
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
-        self.p1_score = 0
-        self.p2_score = 0
+        self.scores = {self.p1: 0, self.p2: 0}
+
+    def show_scores(self):
+        print_pause("Current Scores:")
+        print_pause(f"Player 1: {self.scores[self.p1]}")
+        print_pause(f"Player 2: {self.scores[self.p2]}")
 
     def play_round(self):
-        move1 = self.p1.choose_move()
-        move2 = self.p2.move()
-        print_pause(f"You: {move1}  Player 2: {move2}")
-        if beats(move1, move2):
-            self.p1_score += 1
-            print_pause("You win!")
-        elif beats(move2, move1):
-            self.p2_score += 1
-            print_pause("Player 2 wins!")
-        else:
+        move1, move2 = self.p1.move(), self.p2.move()
+        print_pause(f"Player 1: {move1}  Player 2: {move2}")
+
+        if move1 == move2:
             print_pause("It's a tie!")
-        if hasattr(self.p1, 'learn'):
-            self.p1.learn(move1, move2)
-        if hasattr(self.p2, 'learn'):
-            self.p2.learn(move2, move1)
+        elif beats(move1, move2):
+            self.scores[self.p1] += 1
+            print_pause("Player 1 wins!")
+        else:
+            self.scores[self.p2] += 1
+            print_pause("Player 2 wins!")
+
+        self.show_scores()
+        self.p1.learn(move1, move2)
+        self.p2.learn(move2, move1)
 
     def play_game(self):
-        print_pause("Welcome to Rock, Paper, Scissors!")
-        print_pause("This is a best of 3 match")
-        print_pause("Good Luck")
-        while self.p1_score < 2 and self.p2_score < 2:
+        print_pause("Game start!")
+        round_number = 1
+        while self.scores[self.p1] < 3 and self.scores[self.p2] < 3:
+            print_pause(f"Round {round_number}:")
             self.play_round()
-            print_pause(f"Score:You- {self.p1_score} Player2- {self.p2_score}")
-            print_pause("")
-
-        if self.p1_score > self.p2_score:
-            print_pause("You win the game!")
+            round_number += 1
+        if self.scores[self.p1] > self.scores[self.p2]:
+            print_pause("Player 1 wins the game!")
         else:
             print_pause("Player 2 wins the game!")
         print_pause("Game over!")
-        self.play_again()
 
-    def play_again(self):
-        while True:
-            play_again = input("Play again? (yes/no): ").lower()
-            if play_again == 'yes':
-                new_game = Game(Player(), RandomPlayer())
-                new_game.play_game()
-                return
-            elif play_again == 'no':
-                print_pause("Thanks for playing!")
-                quit()
-            else:
-                print_pause("Invalid input. "
-                            "Please enter 'yes' or 'no'.")
+
+def select_random_computer_player():
+    computer_player_types = [RockerPlayer, RandomPlayer,
+                             MimicPlayer, CyclePlayer]
+    selected_type = random.choice(computer_player_types)
+    return selected_type()
 
 
 if __name__ == '__main__':
-    game = Game(Player(), RandomPlayer())
-    game.play_game()
+    while True:
+        computer_player = select_random_computer_player()
+        game = Game(HumanPlayer(), computer_player)
+        game.play_game()
 
-random_player = RandomPlayer()
-print(random_player.move())
-
-reflect_player = ReflectPlayer()
-print(reflect_player.move())
-
-cycle_player = CyclePlayer()
-print(cycle_player.move())
-
-all_rocker_player = AllRockerPlayer()
-print(all_rocker_player.move())
-
-human_player = HumanPlayer()
-print(human_player.move())
+        replay = input("Do you want to play again? (yes/no): ").lower()
+        if replay != 'yes':
+            break
